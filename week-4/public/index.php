45f5feb55bdaf2d99e9d13674d7fcac9d475bcc7
ashleigh1231 <47766968+ashleigh1231@users.noreply.github.com>
2019-03-23 17:32:49 +1100

    Add files via upload

---
+++ week-4/public/index.php
@@ -0,0 +1,39 @@
+<?php include "template/header.php"; ?>
+
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+   
+    <!-- Font Icon -->
+    <link rel="stylesheet" href="fonts/material-icon/css/material-design-iconic-font.min.css">
+
+    <!-- Main css -->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+    <div class="main">
+
+        <div class="container">
+            <form method="POST" class="appointment-form" id="appointment-form">
+                <h2>Schedule Tracker</h2>
+                <div class="form-group-1">
+                    <input type="text" name="Event" id="Event" placeholder="Event" required />
+                    <input type="text" name="name" id="name" placeholder="Date" required />
+                    <input type="Time" name="Time" id="Time" placeholder="Time" required />
+                    <input type="Details" name="Details" id="Details" placeholder="Details" required />
+                   
+            
+                <div class="form-submit">
+                    <input type="submit" name="submit" id="submit" class="submit" value="Submit" />
+                </div>
+                    
+
+        
+
+   
+<?php include "template/footer.php"; ?>
---
+++ week-4/public/template/header.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+
+    <title>Ashleigh's Collection</title>
+    <meta charset="utf-8">
+
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body>
+    <h1>Schedule Tracker</h1>
+    <ul>
+        <li><a href="create.php">Add a new artwork</a></li>
+        <li><a href="read.php">Find an artwork</a></li>
+        <li><a href="update.php">Update an artwork</a></li>
+        <li><a href="delete.php">Delete an artwork</a></li>
+    </ul>
